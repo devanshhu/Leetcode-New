6f1a27259c600c1f1a6dea2530252faa38e91dfc
Devansh Upadhyay <31622038+devanshhu@users.noreply.github.com>
2022-09-15 17:18:10 +0530

    Added comments in counting bits

--- Bit/338. Counting bits.java
+++ Bit/338. Counting bits.java
@@ -6,6 +6,11 @@ class Solution {
         arr[0] = 0;  
         arr[1] = 1;
         for(int i=2; i < n+1;i++){
+            /*
+                If a no. is even, it will contain the same no. of set bits as its half( Since multiplying an even no. by 2 adds a 0 at the end in it's binary representation, so no set bit is added).
+                If the no. is odd, it will contain 1 more set bit from it's prev no( Since an odd no contains 1 at the end of string & it's prev no. contains a 0 at LSB bcz it was even)
+            
+            */
             arr[i] = i%2 == 0 ? arr[i/2]: arr[i-1]+1;
         }
         return arr;
