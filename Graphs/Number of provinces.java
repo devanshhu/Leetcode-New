e372e1a63aac7686b7dd7ba32ee378e9096abdfc
Devansh Upadhyay <devanshu9719@gmail.com>
2022-09-24 14:20:17 +0530

    Added NUmber of provinces

---
+++ Graphs/Number of provinces.java
@@ -0,0 +1,68 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            int V = Integer.parseInt(read.readLine());
+            
+            ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+            
+            for(int i=0; i<V; i++)
+            {
+                String S[] = read.readLine().split(" ");
+                ArrayList<Integer> temp = new ArrayList<>();
+                for(int j=0; j<V; j++)
+                    temp.add(Integer.parseInt(S[j]));
+                adj.add(temp);
+            }
+
+            Solution ob = new Solution();
+            System.out.println(ob.numProvinces(adj,V));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    static int numProvinces(ArrayList<ArrayList<Integer>> adj, int V) {
+        // code here
+        ArrayList<ArrayList<Integer>> list = new ArrayList<>();
+        for(int i=0;i< V;i++){
+            list.add(new ArrayList<Integer>());
+        }
+        for(int i=0;i<V;i++){
+            for(int j=0;j<V;j++){
+                if(i != j && adj.get(i).get(j) == 1){
+                    list.get(i).add(j);
+                    list.get(j).add(i);
+                }
+            }
+            
+        }
+        int[] visited = new int[V];
+        int count =0;
+        for(int i=0;i<V;i++){
+            if(visited[i] == 0){
+                count++;
+                dfs(i, visited,list);
+             }
+        }
+        return count;
+    }
+    
+    static void dfs(int node, int[] visited, ArrayList<ArrayList<Integer>> adj){
+        if(visited[node] == 1) return;
+        visited[node] = 1;
+        for(int i: adj.get(node)){
+            dfs(i, visited, adj);
+        }
+    }
+};
