8450c933fca69a0ddf16edb92de620221549d382
Devansh Upadhyay <31622038+devanshhu@users.noreply.github.com>
2022-10-05 16:56:49 +0530

    Update checkBipartite to add DFS traversal

--- Graphs/Check Bipartite.java
+++ Graphs/Check Bipartite.java
@@ -7,12 +7,12 @@ class Solution
         Arrays.fill(color, -1);
         for(int i=0;i<V;i++){
             if(color[i] == -1)
-                if(!checkBipartite(i, V, color, adj )) return false;
+                if(!checkBipartiteBFS(i, V, color, adj )) return false;
         }
         return true;
     }
     
-    public boolean checkBipartite(int src, int V, int[] color, ArrayList<ArrayList<Integer>>adj){
+    public boolean checkBipartiteBFS(int src, int V, int[] color, ArrayList<ArrayList<Integer>>adj){
         Queue<Integer> q = new LinkedList();
         color[src] = 0;
         q.add(src);
@@ -26,6 +26,15 @@ class Solution
             }
         }
         return true;
-        
     }
+    public boolean checkBipartiteDfs(int src,int col, int[] color, ArrayList<ArrayList<Integer>>adj){
+        color[src] = col;
+        for(int it: adj.get(src)){
+            if(color[it] == -1){
+                if(checkBipartiteDfs(it,1-col,color, adj) == false) return false;
+            }else if(color[it] == col) return false;
+        }
+        
+        return true;
+      }
 }
